12c9b7a36f26b8026b026b431c439b73c4152496
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-09 12:57:02 -0600

    feat(component): componente principal del formulario de produccion

---
+++ src/pages/Production/Production.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { ProductionForm } from "../../components/Production/ProductionForm";
+
+export const Production = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [auditHeader, setAuditHeader] = useState<{
+    auditorName: string;
+    selectedArea: string;
+  } | null>(null);
+
+  const handleInitialDataSubmit = (data: {
+    auditorName: string;
+    selectedArea: string;
+  }) => {
+    setAuditHeader(data);
+    console.log("Datos listos para el Paso 2:", data);
+
+    setCurrentStep(2);
+  };
+
+  return (
+    <div
+      className="w-full min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center 
+      bg-slate-50 p-4"
+    >
+      {currentStep === 1 && (
+        <ProductionForm onNextStep={handleInitialDataSubmit} />
+      )}
+
+      {currentStep === 2 && (
+        <div className="architecture-fade-in text-center p-8 bg-white rounded-2xl shadow">
+          <h3 className="text-xl font-bold text-slate-800">
+            Paso 2: Evaluación de las 5S
+          </h3>
+          <p className="text-slate-500 mt-2">
+            Aquí renderizaremos el checklist para la línea:{" "}
+            {auditHeader?.selectedArea}
+          </p>
+          <p className="text-sm text-slate-400 mt-1">
+            Auditor: {auditHeader?.auditorName}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
