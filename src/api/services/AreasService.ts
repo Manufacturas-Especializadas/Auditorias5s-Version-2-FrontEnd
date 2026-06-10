0ac57e31d49d432703312a2d22f101620ac984a2
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-10 15:18:16 -0600

    feat(service): metodo para obtener por id - areas

--- src/api/services/AreasService.ts
+++ src/api/services/AreasService.ts
@@ -1,11 +1,16 @@
 import { API_CONFIG } from "../../config/api";
-import type { CreateAreas } from "../../types/Types";
+import type { AllAreas, CreateAreas } from "../../types/Types";
 import { apiClient } from "../client";
 
 class AreasServices {
+  private getByIdEndpoint = API_CONFIG.endpoints.areas.getById;
   private createEndpoint = API_CONFIG.endpoints.areas.create;
   private updateEndpoint = API_CONFIG.endpoints.areas.update;
   private deleteEndpoint = API_CONFIG.endpoints.areas.delete;
+
+  async getBy(id: number): Promise<AllAreas[]> {
+    return apiClient.get<AllAreas[]>(`${this.getByIdEndpoint}${id}`);
+  }
 
   async create(data: CreateAreas): Promise<void> {
     return apiClient.post<void>(this.createEndpoint, data);
