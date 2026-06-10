3c567de71dd6b47d85029168ea8d50ab0cf01ccd
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-10 10:49:20 -0600

    feat(service): metodo para obtener los modulos

--- src/api/services/ModuleService.ts
+++ src/api/services/ModuleService.ts
@@ -1,3 +1,13 @@
-class ModuleServce {}
+import { API_CONFIG } from "../../config/api";
+import type { AllModule } from "../../types/Types";
+import { apiClient } from "../client";
+
+class ModuleServce {
+  private getAllEndpoint = API_CONFIG.endpoints.modules.getAll;
+
+  async getAll(): Promise<AllModule[]> {
+    return apiClient.get<AllModule[]>(this.getAllEndpoint);
+  }
+}
 
 export const moduleService = new ModuleServce();
