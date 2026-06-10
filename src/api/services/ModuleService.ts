f147e8a0efb9b9c663f5f8ea2ff744ac72ed22d6
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-10 11:36:10 -0600

    feat(service): metodo update - modulos

--- src/api/services/ModuleService.ts
+++ src/api/services/ModuleService.ts
@@ -5,6 +5,7 @@ import { apiClient } from "../client";
 class ModuleServce {
   private getAllEndpoint = API_CONFIG.endpoints.modules.getAll;
   private createEndpoint = API_CONFIG.endpoints.modules.create;
+  private updateEndpoint = API_CONFIG.endpoints.modules.update;
 
   async getAll(): Promise<AllModule[]> {
     return apiClient.get<AllModule[]>(this.getAllEndpoint);
@@ -12,6 +13,18 @@ class ModuleServce {
 
   async create(name: string): Promise<any> {
     return apiClient.post<any>(this.createEndpoint, { name });
+  }
+
+  async update(
+    moduleId: number,
+    name: string,
+    isActive: boolean,
+  ): Promise<any> {
+    return apiClient.put<any>(this.updateEndpoint, {
+      moduleId,
+      name,
+      isActive,
+    });
   }
 }
 
