18d4d683836914f227bfc052623767030261b2aa
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-10 11:19:12 -0600

    feat(service): metodo create - modulos

--- src/api/services/ModuleService.ts
+++ src/api/services/ModuleService.ts
@@ -1,12 +1,17 @@
 import { API_CONFIG } from "../../config/api";
-import type { AllModule } from "../../types/Types";
+import type { AllModule, CreateModule } from "../../types/Types";
 import { apiClient } from "../client";
 
 class ModuleServce {
   private getAllEndpoint = API_CONFIG.endpoints.modules.getAll;
+  private createEndpoint = API_CONFIG.endpoints.modules.create;
 
   async getAll(): Promise<AllModule[]> {
     return apiClient.get<AllModule[]>(this.getAllEndpoint);
+  }
+
+  async create(data: CreateModule): Promise<void> {
+    return apiClient.post<void>(this.createEndpoint, data);
   }
 }
 
