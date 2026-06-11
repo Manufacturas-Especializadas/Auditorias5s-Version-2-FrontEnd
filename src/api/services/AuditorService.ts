dfb1d5f2eb3d25bda0c1a628d31b7c118007f069
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-11 08:39:42 -0600

    feat(service): operaciones CRUD - Auditores

--- src/api/services/AuditorService.ts
+++ src/api/services/AuditorService.ts
@@ -1,3 +1,23 @@
-class AuditorService {}
+import { API_CONFIG } from "../../config/api";
+import type { AllAuditor } from "../../types/Types";
+import { apiClient } from "../client";
+
+class AuditorService {
+  private getAllEndpoint = API_CONFIG.endpoints.auditors.getAll;
+  private createEndpoint = API_CONFIG.endpoints.auditors.create;
+  private deleteEndpiint = API_CONFIG.endpoints.auditors.delete;
+
+  async getAll(): Promise<AllAuditor[]> {
+    return apiClient.get<AllAuditor[]>(this.getAllEndpoint);
+  }
+
+  async create(fullName: string): Promise<any> {
+    return apiClient.post<any>(this.createEndpoint, { fullName });
+  }
+
+  async delete(id: number): Promise<any> {
+    return apiClient.delete<any>(`${this.deleteEndpiint}${id}`);
+  }
+}
 
 export const auditorService = new AuditorService();
